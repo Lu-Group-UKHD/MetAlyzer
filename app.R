ddf7bc1283e0eace8417afd460bdfd12b5434e6e
LiaoQianWu <qianwu.liao@gmail.com>
2023-12-22 10:42:41 +0100

    Finalize feature and sample filtering functions

--- app.R
+++ app.R
@@ -6,32 +6,32 @@ library(tidyverse)
 # setwd('/Users/qianwu/Desktop/RShiny_Biocrates_DataAnalysis')
 # metabObj <- MetAlyzer_dataset(file_path = './data/extraction_data.xlsx', silent = T)
 
-ui <- shiny::fluidPage(
+ui <- fluidPage(
   # App title
-  shiny::titlePanel('Biocrates Data Analysis'),
+  titlePanel('Biocrates Data Analysis'),
   
-  shiny::tabsetPanel(
+  tabsetPanel(
     type = 'tabs',
-    shiny::tabPanel(
+    tabPanel(
       'Preprocessing',
-      shiny::sidebarLayout(
-        shiny::sidebarPanel(
-          shiny::fileInput('uploadedFile', 'Upload xlsx file:',
-                           multiple = F, accept = '.xlsx'),
-          shiny::selectInput('featFiltering', 'Select feature(s) to remove:',
-                             choices = character(0), multiple = T),
-          shiny::selectInput('smpFiltering', 'Select sample(s) to remove:',
-                             choices = character(0), multiple = T),
-          shiny::actionButton('updateFiltering', 'Filter', width = '49%'),
-          shiny::actionButton('resetFiltering', 'Reset', width = '49%')
+      sidebarLayout(
+        sidebarPanel(
+          fileInput('uploadedFile', 'Upload xlsx file:', multiple = F, accept = '.xlsx'),
+          # Show filtering options only after a file is uploaded
+          conditionalPanel(condition = "output.if_uploadedFile",
+                           selectInput('featFiltering', 'Select feature(s) to remove:',
+                                       choices = character(0), multiple = T),
+                           selectInput('smpFiltering', 'Select sample(s) to remove:',
+                                       choices = character(0), multiple = T),
+                           actionButton('updateFiltering', 'Filter', width = '49%'),
+                           actionButton('resetFiltering', 'Reset', width = '49%'))
         ),
-        shiny::mainPanel(
-          shiny::tableOutput('outputId'),
-          shiny::textOutput('testing')
+        mainPanel(
+          tableOutput('outputId')
         )
       )
     ),
-    shiny::tabPanel(
+    tabPanel(
       'Visualization',
       tags$h4('Could be a section for data visualization')
     )
@@ -39,70 +39,138 @@ ui <- shiny::fluidPage(
 )
 
 server <- function(input, output, session) {
-  # Create reactive variables
-  reactObjs <- shiny::reactiveValues(metabObj = NULL, ori_metabObj = NULL)
+  # Create reactive object for storing uploaded file
+  reactObjs <- reactiveValues(metabObj = NULL, ori_metabObj = NULL)
+  
+  # Monitor whether file is uploaded to show further operations on client side
+  output$if_uploadedFile <- reactive({
+    !is.null(input$uploadedFile)
+  })
+  outputOptions(output, 'if_uploadedFile', suspendWhenHidden = F)
   
   # Initialize MetAlyzer SE object
-  shiny::observeEvent(input$uploadedFile, {
+  observeEvent(input$uploadedFile, {
     reactObjs$metabObj <- MetAlyzer_dataset(file_path = input$uploadedFile$datapath,
                                             sheet = 1, silent = T)
-    # Make a copy of original data for filtering reset
+    # Make copy of original data for filtering reset
     reactObjs$ori_metabObj <- reactObjs$metabObj
   })
   
+  
+  # Prepare choices for feature filtering
+  featChoices <- reactive({
+    list(Class = as.list(unique(rowData(reactObjs$metabObj)$metabolic_classes)),
+         Metabolite = as.list(rownames(reactObjs$metabObj)))
+    
+  })
+  # Prepare choices for sample filtering
+  smpChoices <- reactive({
+    tmp_smpChoices <- as.list(colData(reactObjs$metabObj))
+    # Uniquify choices in every choice group and convert NA into character so
+    # that it can be shown on client side
+    for (i in seq_len(length(tmp_smpChoices))) {
+      choices <- unique(tmp_smpChoices[[i]])
+      if (any(is.na(choices))) {
+        choices <- choices[-which(is.na(choices))]
+        tmp_smpChoices[[i]] <- c(choices, 'NA')
+      } else {
+        tmp_smpChoices[[i]] <- choices
+      }
+    }
+    # Remove choice groups with only one level
+    # rm_choiceGps <- which(sapply(tmp_smpChoices, length) == 1)
+    # if (length(rm_choiceGps) != 0) {
+    #   tmp_smpChoices <- tmp_smpChoices[-rm_choiceGps]
+    # }
+    # Make identical choices in different choice groups unique by adding suffixes
+    all_smpChoices <- unlist(tmp_smpChoices)
+    dup_smpChoices <- all_smpChoices[duplicated(all_smpChoices)]
+    # Collect choice groups with duplicated choices for later choice group searches
+    # (due to design of MetAlyzer::filterMetaData)
+    dup_smpChoiceGps <- c()
+    if (length(dup_smpChoices) != 0) {
+      for (i in seq_len(length(tmp_smpChoices))) {
+        choices <- tmp_smpChoices[[i]]
+        if (any(dup_smpChoices %in% choices)) {
+          dupIdx <- which(choices %in% dup_smpChoices)
+          choices[dupIdx] <- paste0(choices[dupIdx], ' (', names(tmp_smpChoices)[i], ')')
+          tmp_smpChoices[[i]] <- choices
+          dup_smpChoiceGps <- c(dup_smpChoiceGps, names(tmp_smpChoices)[i])
+        }
+      }
+    }
+    # Make choices lists so that sole choice in certain choice group can be shown
+    tmp_smpChoices <- lapply(tmp_smpChoices, function(choices) {
+      as.list(choices)
+    })
+    
+    # Create dictionary for later choice group searches of certain choices (due
+    # to design of MetAlyzer::filterMetaData)
+    choiceGpSizes <- sapply(tmp_smpChoices, length)
+    tmp_smpChoices2Gps <- lapply(seq_len(length(choiceGpSizes)), function(i) {
+      rep(names(choiceGpSizes)[i], choiceGpSizes[i])
+    })
+    tmp_smpChoices2Gps <- unlist(tmp_smpChoices2Gps)
+    names(tmp_smpChoices2Gps) <- unlist(tmp_smpChoices)
+    
+    return(list(smpChoices = tmp_smpChoices, smpChoices2Gps = tmp_smpChoices2Gps,
+                dup_smpChoiceGps = dup_smpChoiceGps))
+  })
   # Update choices for feature and sample filtering
   observe({
-    # Stop proceeding if file has not been uploaded. Current calculation (reactive
-    # expression) and any callers on it will be stopped silently (avoid error when
-    # app is just initiated)
-    shiny::req(input$uploadedFile)
-    # Feature filtering choices
-    featChoices <- list(Class = unique(rowData(reactObjs$metabObj)$metabolic_classes),
-                        Metabolite = rownames(reactObjs$metabObj))
-    shiny::updateSelectInput(session, 'featFiltering', choices = featChoices)
-    # Sample filtering choices
-    smpChoices <- lapply(as.list(colData(reactObjs$metabObj)), function(metaVar) {
-      metaVar <- unique(metaVar)
-      if (all(!is.na(metaVar))) {
-        metaVar
-      } else {
-        # Convert NA into a character so that it can be shown on client side
-        #### Not only are levels of metadata variables needed but also metadata
-        #### variables. Same levels of different variables (e.g., 'NA') have to
-        #### be shown. Try having two select boxes using 'conditionalPanel'?
-        metaVar <- metaVar[-which(is.na(metaVar))]
-        c(metaVar, 'NA')
-      }
-    })
-    # Remove options with only one level
-    smpChoices <- smpChoices[-which(sapply(smpChoices, length) == 1)]
-    shiny::updateSelectInput(session, 'smpFiltering', choices = smpChoices)
+    req(input$uploadedFile)
+    # For feature filtering
+    if ('Metabolism Indicators' %in% unlist(featChoices()) &
+        nrow(reactObjs$metabObj) == nrow(reactObjs$ori_metabObj)) {
+      updateSelectInput(session, 'featFiltering', choices = featChoices(),
+                        selected = 'Metabolism Indicators')
+    } else {
+      updateSelectInput(session, 'featFiltering', choices = featChoices())
+    }
+    # For sample filtering
+    updateSelectInput(session, 'smpFiltering', choices = smpChoices()$smpChoices)
   })
-  
   # Do feature and sample filtering
-  shiny::observeEvent(input$updateFiltering, {
-    #### There are many more filtering criteria in function to explore
-    if (!is.null(reactObjs$metabObj)) {
-      # Feature filtering
-      reactObjs$metabObj <- MetAlyzer::filterMetabolites(reactObjs$metabObj,
-                                                         drop_metabolites = input$featFiltering)
-      # Sample filtering
-      # if (input$smpFiltering == 'NA') {
-      #   reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj, is.na(input$ABC))
-      # }
+  observeEvent(input$updateFiltering, {
+    # For feature filtering
+    #### Probably add slider for 'min_percent_valid' and options for 'per_group'
+    #### to filter unmet features out by threshold of valid values
+    reactObjs$metabObj <- MetAlyzer::filterMetabolites(reactObjs$metabObj,
+                                                       drop_metabolites = input$featFiltering)
+    # For sample filtering
+    for (selectChoice in input$smpFiltering) {
+      selectChoiceGp <- smpChoices()$smpChoices2Gps[selectChoice]
+      if (!selectChoiceGp %in% smpChoices()$dup_smpChoiceGps) {
+        if (selectChoice == 'NA') {
+          reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj,
+                                                          !is.na(.data[[selectChoiceGp]]))
+        } else {
+          reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj,
+                                                          .data[[selectChoiceGp]] != selectChoice)
+        }
+      } else {
+        # Prepare manually added suffix if selected choice group is in collected
+        # choice groups with duplicates (see smpChoices() section)
+        regexSuffix <- paste0(' \\(', selectChoiceGp, '\\)')
+        tmp_selectChoice <- stringr::str_remove(selectChoice, regexSuffix)
+        if (tmp_selectChoice == 'NA') {
+          reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj,
+                                                          !is.na(.data[[selectChoiceGp]]))
+        } else {
+          reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj,
+                                                          .data[[selectChoiceGp]] != tmp_selectChoice)
+        }
+      }
     }
   })
   # Reset feature and sample filtering
-  shiny::observeEvent(input$resetFiltering, {
+  observeEvent(input$resetFiltering, {
     reactObjs$metabObj <- reactObjs$ori_metabObj
   })
   
-  output$outputId <- shiny::renderTable({
-    dim(reactObjs$metabObj)
-  })
   
-  output$testing <- shiny::renderText({
-    input$smpFiltering
+  output$outputId <- renderTable({
+    dim(reactObjs$metabObj)
   })
 }
 
