160cdbe6a7228da89b09afa3c9a01dd3e984a518
LiaoQianWu <qianwu.liao@gmail.com>
2024-01-02 12:11:04 +0100

    Add pop-up message if reading uploaded file is failed and modularize feature and sample filtering

--- app.R
+++ app.R
@@ -8,7 +8,7 @@ library(tidyverse)
 
 ui <- fluidPage(
   # App title
-  titlePanel('Biocrates Data Analysis'),
+  titlePanel('Biocrates Metabolomics Data Analysis'),
   
   tabsetPanel(
     type = 'tabs',
@@ -18,7 +18,7 @@ ui <- fluidPage(
         sidebarPanel(
           fileInput('uploadedFile', 'Upload xlsx file:', multiple = F, accept = '.xlsx'),
           # Show filtering options only after a file is uploaded
-          conditionalPanel(condition = "output.if_uploadedFile",
+          conditionalPanel(condition = "output.ifValidUploadedFile",
                            selectInput('featFiltering', 'Select feature(s) to remove:',
                                        choices = character(0), multiple = T),
                            selectInput('smpFiltering', 'Select sample(s) to remove:',
@@ -27,6 +27,10 @@ ui <- fluidPage(
                            actionButton('resetFiltering', 'Reset', width = '49%'))
         ),
         mainPanel(
+          fluidRow(
+            column(width = 7, verbatimTextOutput('textConcSumm')),
+            column(width = 5, verbatimTextOutput('textQuanSumm'))
+          ),
           tableOutput('outputId')
         )
       )
@@ -42,84 +46,98 @@ server <- function(input, output, session) {
   # Create reactive object for storing uploaded file
   reactObjs <- reactiveValues(metabObj = NULL, ori_metabObj = NULL)
   
-  # Monitor whether file is uploaded to show further operations on client side
-  output$if_uploadedFile <- reactive({
-    !is.null(input$uploadedFile)
-  })
-  outputOptions(output, 'if_uploadedFile', suspendWhenHidden = F)
-  
   # Initialize MetAlyzer SE object
   observeEvent(input$uploadedFile, {
-    reactObjs$metabObj <- MetAlyzer_dataset(file_path = input$uploadedFile$datapath,
-                                            sheet = 1, silent = T)
-    # Make copy of original data for filtering reset
-    reactObjs$ori_metabObj <- reactObjs$metabObj
+    validUploadedFile <- try(
+      metabObj <- MetAlyzer_dataset(file_path = input$uploadedFile$datapath,
+                                    sheet = 1, silent = T),
+      silent = T)
+    if (!is(validUploadedFile, 'try-error')) {
+      reactObjs$metabObj <- metabObj
+      # Make copy of original data for filtering reset
+      reactObjs$ori_metabObj <- metabObj
+      # Monitor whether uploaded file is valid to show further operations on client side
+      output$ifValidUploadedFile <- reactive({
+        !is.null(reactObjs$metabObj)
+      })
+      outputOptions(output, 'ifValidUploadedFile', suspendWhenHidden = F)
+    } else {
+      showModal(modalDialog(
+        title = 'Uploaded file reading failed...',
+        'Is the uploaded xlsx file generated from the MetIDQ™ software?',
+        easyClose = T,
+        footer = NULL
+      ))
+      reactObjs$metabObj <- NULL
+      reactObjs$ori_metabObj <- NULL
+    }
   })
   
   
   # Prepare choices for feature filtering
   featChoices <- reactive({
+    req(reactObjs$metabObj)
     list(Class = as.list(unique(rowData(reactObjs$metabObj)$metabolic_classes)),
          Metabolite = as.list(rownames(reactObjs$metabObj)))
-    
   })
-  # Prepare choices for sample filtering
-  smpChoices <- reactive({
-    tmp_smpChoices <- as.list(colData(reactObjs$metabObj))
+  # Prepare needed information for sample filtering (i.e., choices, dictionary for
+  # choice group searches, and list of choice groups containing duplicated choices)
+  smpFilterList <- reactive({
+    req(reactObjs$metabObj)
+    smpChoices <- as.list(colData(reactObjs$metabObj))
     # Uniquify choices in every choice group and convert NA into character so
     # that it can be shown on client side
-    for (i in seq_len(length(tmp_smpChoices))) {
-      choices <- unique(tmp_smpChoices[[i]])
+    for (i in seq_len(length(smpChoices))) {
+      choices <- unique(smpChoices[[i]])
       if (any(is.na(choices))) {
         choices <- choices[-which(is.na(choices))]
-        tmp_smpChoices[[i]] <- c(choices, 'NA')
+        smpChoices[[i]] <- c(choices, 'NA')
       } else {
-        tmp_smpChoices[[i]] <- choices
+        smpChoices[[i]] <- choices
       }
     }
     # Remove choice groups with only one level
-    # rm_choiceGps <- which(sapply(tmp_smpChoices, length) == 1)
-    # if (length(rm_choiceGps) != 0) {
-    #   tmp_smpChoices <- tmp_smpChoices[-rm_choiceGps]
+    # rmChoiceGps <- which(sapply(smpChoices, length) == 1)
+    # if (length(rmChoiceGps) != 0) {
+    #   smpChoices <- smpChoices[-rmChoiceGps]
     # }
     # Make identical choices in different choice groups unique by adding suffixes
-    all_smpChoices <- unlist(tmp_smpChoices)
-    dup_smpChoices <- all_smpChoices[duplicated(all_smpChoices)]
+    allSmpChoices <- unlist(smpChoices)
+    dupSmpChoices <- allSmpChoices[duplicated(allSmpChoices)]
     # Collect choice groups with duplicated choices for later choice group searches
     # (due to design of MetAlyzer::filterMetaData)
-    dup_smpChoiceGps <- c()
-    if (length(dup_smpChoices) != 0) {
-      for (i in seq_len(length(tmp_smpChoices))) {
-        choices <- tmp_smpChoices[[i]]
-        if (any(dup_smpChoices %in% choices)) {
-          dupIdx <- which(choices %in% dup_smpChoices)
-          choices[dupIdx] <- paste0(choices[dupIdx], ' (', names(tmp_smpChoices)[i], ')')
-          tmp_smpChoices[[i]] <- choices
-          dup_smpChoiceGps <- c(dup_smpChoiceGps, names(tmp_smpChoices)[i])
+    dupSmpChoiceGps <- c()
+    if (length(dupSmpChoices) != 0) {
+      for (i in seq_len(length(smpChoices))) {
+        choices <- smpChoices[[i]]
+        if (any(dupSmpChoices %in% choices)) {
+          dupIdx <- which(choices %in% dupSmpChoices)
+          choices[dupIdx] <- paste0(choices[dupIdx], ' (', names(smpChoices)[i], ')')
+          smpChoices[[i]] <- choices
+          dupSmpChoiceGps <- c(dupSmpChoiceGps, names(smpChoices)[i])
         }
       }
     }
     # Make choices lists so that sole choice in certain choice group can be shown
-    tmp_smpChoices <- lapply(tmp_smpChoices, function(choices) {
+    smpChoices <- lapply(smpChoices, function(choices) {
       as.list(choices)
     })
     
     # Create dictionary for later choice group searches of certain choices (due
     # to design of MetAlyzer::filterMetaData)
-    choiceGpSizes <- sapply(tmp_smpChoices, length)
-    tmp_smpChoices2Gps <- lapply(seq_len(length(choiceGpSizes)), function(i) {
+    choiceGpSizes <- sapply(smpChoices, length)
+    smpChoices2Gps <- lapply(seq_len(length(choiceGpSizes)), function(i) {
       rep(names(choiceGpSizes)[i], choiceGpSizes[i])
     })
-    tmp_smpChoices2Gps <- unlist(tmp_smpChoices2Gps)
-    names(tmp_smpChoices2Gps) <- unlist(tmp_smpChoices)
+    smpChoices2Gps <- unlist(smpChoices2Gps)
+    names(smpChoices2Gps) <- unlist(smpChoices)
     
-    return(list(smpChoices = tmp_smpChoices, smpChoices2Gps = tmp_smpChoices2Gps,
-                dup_smpChoiceGps = dup_smpChoiceGps))
+    return(list(smpChoices = smpChoices, smpChoices2Gps = smpChoices2Gps,
+                dupSmpChoiceGps = dupSmpChoiceGps))
   })
-  # Update choices for feature and sample filtering
+  # Update choices for feature filtering
   observe({
-    req(input$uploadedFile)
-    # For feature filtering
+    req(featChoices())
     if ('Metabolism Indicators' %in% unlist(featChoices()) &
         nrow(reactObjs$metabObj) == nrow(reactObjs$ori_metabObj)) {
       updateSelectInput(session, 'featFiltering', choices = featChoices(),
@@ -127,20 +145,30 @@ server <- function(input, output, session) {
     } else {
       updateSelectInput(session, 'featFiltering', choices = featChoices())
     }
-    # For sample filtering
-    updateSelectInput(session, 'smpFiltering', choices = smpChoices()$smpChoices)
   })
-  # Do feature and sample filtering
+  # Update choices for sample filtering
+  observe({
+    req(smpFilterList()$smpChoices)
+    updateSelectInput(session, 'smpFiltering', choices = smpFilterList()$smpChoices)
+  })
+  # Do feature filtering
   observeEvent(input$updateFiltering, {
-    # For feature filtering
-    #### Probably add slider for 'min_percent_valid' and options for 'per_group'
-    #### to filter unmet features out by threshold of valid values
-    reactObjs$metabObj <- MetAlyzer::filterMetabolites(reactObjs$metabObj,
-                                                       drop_metabolites = input$featFiltering)
-    # For sample filtering
+    if (!is.null(input$featFiltering)) {
+      #### Probably add slider for 'min_percent_valid' and options for 'per_group'
+      #### to filter unmet features out by threshold of valid values
+      reactObjs$metabObj <- MetAlyzer::filterMetabolites(reactObjs$metabObj,
+                                                         drop_metabolites = input$featFiltering)
+    } else {
+      reactObjs$metabObj <- MetAlyzer::filterMetabolites(reactObjs$metabObj,
+                                                         drop_metabolites = NULL)
+    }
+  })
+  # Do sample filtering
+  observeEvent(input$updateFiltering, {
+    req(input$smpFiltering)
     for (selectChoice in input$smpFiltering) {
-      selectChoiceGp <- smpChoices()$smpChoices2Gps[selectChoice]
-      if (!selectChoiceGp %in% smpChoices()$dup_smpChoiceGps) {
+      selectChoiceGp <- smpFilterList()$smpChoices2Gps[selectChoice]
+      if (!selectChoiceGp %in% smpFilterList()$dupSmpChoiceGps) {
         if (selectChoice == 'NA') {
           reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj,
                                                           !is.na(.data[[selectChoiceGp]]))
@@ -150,15 +178,15 @@ server <- function(input, output, session) {
         }
       } else {
         # Prepare manually added suffix if selected choice group is in collected
-        # choice groups with duplicates (see smpChoices() section)
+        # choice groups with duplicates (see smpFilterList() section)
         regexSuffix <- paste0(' \\(', selectChoiceGp, '\\)')
-        tmp_selectChoice <- stringr::str_remove(selectChoice, regexSuffix)
-        if (tmp_selectChoice == 'NA') {
+        modSelectChoice <- stringr::str_remove(selectChoice, regexSuffix)
+        if (modSelectChoice == 'NA') {
           reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj,
                                                           !is.na(.data[[selectChoiceGp]]))
         } else {
           reactObjs$metabObj <- MetAlyzer::filterMetaData(reactObjs$metabObj,
-                                                          .data[[selectChoiceGp]] != tmp_selectChoice)
+                                                          .data[[selectChoiceGp]] != modSelectChoice)
         }
       }
     }
@@ -168,6 +196,16 @@ server <- function(input, output, session) {
     reactObjs$metabObj <- reactObjs$ori_metabObj
   })
   
+  
+  # Show statistics of data concentration values and quantification statuses 
+  output$textConcSumm <- renderPrint({
+    req(reactObjs$metabObj)
+    MetAlyzer::summarizeConcValues(reactObjs$metabObj)
+  })
+  output$textQuanSumm <- renderPrint({
+    req(reactObjs$metabObj)
+    MetAlyzer::summarizeQuantData(reactObjs$metabObj)
+  })
   
   output$outputId <- renderTable({
     dim(reactObjs$metabObj)
