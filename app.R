7c6b5a4033cdf9780965ed25062725f4ff5bf709
LiaoQianWu <qianwu.liao@gmail.com>
2024-01-09 15:55:32 +0100

    Revert "integrated the plotly plots into GUI"
    This reverts commit 873f1a054e21e68e2d6bf55cede652af86095c08.
    
    Revert last changes of volcano and scatter plots in app.R
    
    Instead of coding directly in app.R, functions of making volcano and scatter plots will be created in other .R files and called in app.R.

--- app.R
+++ app.R
@@ -56,11 +56,8 @@ ui <- fluidPage(
             column(width = 5, verbatimTextOutput('textQuanSumm'))
           ),
           tableOutput('outputId'),
-          plotlyOutput('plotVolcano'),
-          fluidRow(
-            column(width = 9, plotlyOutput('plotScatter')),
-            column(width = 3, plotOutput('plotScatterLegend'))
-          ),
+          plotOutput('plotVolcano'),
+          plotOutput('plotScatter'),
           plotOutput('plotNetwork')
         )
       )
@@ -76,9 +73,6 @@ server <- function(input, output, session) {
   # Create reactive objects for storing up-to-date data
   reactMetabObj <- reactiveValues(metabObj = NULL, ori_metabObj = NULL)
   reactLog2FCTab <- reactiveVal()
-  reactLog2FCvulcano <- reactiveVal()
-  reactP_data <- reactiveVal()
-  reactRects_df <- reactiveVal()
   
   # Initialize MetAlyzer SE object
   observeEvent(input$uploadedFile, {
@@ -297,134 +291,6 @@ server <- function(input, output, session) {
       log2FCTab <- do.call(MetAlyzer::calculate_log2FC, list(metalyzer_se = metabObj,
                                                              categorical = as.symbol(selectedChoiceGp),
                                                              impute_perc_of_min = 0.2, impute_NA = T))
-
-      
-      ### Server Side Data Wrangling
-      ## Background: Define colors for significance 
-      signif_colors=c("#5F5F5F"=1,
-                      "#FEBF6E"=0.1,
-                      "#EE5C42"=0.05,
-                      "#8B1A1A"=0.01)
-      
-      ## Background: Load polarity data
-      polarity_file <- system.file("extdata", "polarity.csv", package = "MetAlyzer")
-      
-      polarity_df <- utils::read.csv(polarity_file) %>%
-        select(.data$Class,
-               .data$Polarity) %>%
-        mutate(Class = factor(.data$Class),
-               Polarity = factor(.data$Polarity, levels = c('LC', 'FIA'))) %>%
-        arrange(.data$Polarity)
-      
-      ## Background: Set class colors
-      class_colors <- metalyzer_colors()
-      
-      names(class_colors) <- levels(polarity_df$Class)
-      
-      ## Background: Define LC and FIA classes with color
-      lc_polarity_df <- filter(polarity_df,
-                               .data$Polarity == 'LC',
-                               .data$Class %in% log2FCTab$Class)
-      lc_colors <- class_colors[which(names(class_colors) %in% lc_polarity_df$Class)]
-      fia_polarity_df <- filter(polarity_df,
-                                .data$Polarity == 'FIA',
-                                .data$Class %in% log2FCTab$Class)
-      fia_colors <- class_colors[which(names(class_colors) %in% fia_polarity_df$Class)]
-      
-      ## Data: Replace NAs
-      log2FCTab$log2FC[is.na(log2FCTab$log2FC)] <- 0
-      log2FCTab$qval[is.na(log2FCTab$qval)] <- 1
-      
-      # Data Vulcano: Create Dataframe for vulcano plot
-      log2FCvulcano <- log2FCTab
-      log2FCvulcano$Class <- as.character(log2FCvulcano$Class)
-      log2FCvulcano$Class[log2FCvulcano$qval > 0.05] <- NA
-      log2FCvulcano$Class[abs(log2FCvulcano$log2FC) < log2(1.5)] <- NA
-
-      log2FCvulcano$labels <- as.character(log2FCvulcano$Metabolite)
-      log2FCvulcano$labels[which(is.na(log2FCvulcano$Class))] <- ""
-      
-      ## Data Scatter: Add color to data based on significance
-      log2FCTab$signif_color <- sapply(log2FCTab$qval, function(q_val) {
-        for (t in signif_colors) {
-          if (q_val <= t) {
-            color <- names(signif_colors)[which(signif_colors == t)]
-          }
-        }
-        return(color)
-      })
-      
-      ## Data Scatter: Add pseudo x-value to data as a order of metabolites
-      ordered_classes <- c(names(lc_colors), names(fia_colors))
-      p_data <- lapply(ordered_classes, function(class) {
-        log2FCTab %>%
-          filter(.data$Class == class) %>%
-          bind_rows(data.frame(Class = rep(NA, 5)))
-      }) %>%
-        bind_rows()
-      p_data <- bind_rows(data.frame(Class = rep(NA, 5)), p_data)
-      p_data$x <- seq(nrow(p_data))
-      p_data <- filter(p_data, !is.na(.data$Class))
-      
-      ## Data Scatter: Determine labels
-      signif_p_data <- filter(p_data, .data$signif_color != names(signif_colors)[1])
-      
-      labels <- sapply(p_data$Metabolite, function(m) {
-        m <- as.character(m)
-        label <- if_else(m %in% signif_p_data$Metabolite, m, "")
-        return(label)
-      })
-      
-      ## Legend Scatter: Significance color
-      signif_colors <- sort(signif_colors, decreasing = TRUE)
-      signif_labels <- list()
-      for (i in seq_along(signif_colors)) {
-        t <- signif_colors[i]
-        names(t) <- NULL
-        if (i < length(signif_colors)) {
-          t2 <- signif_colors[i+1]
-          names(t2) <- NULL
-          label <- bquote(.(t) ~ "\u2265 q-value >" ~ .(t2))
-        } else {
-          label <- bquote(.(t) ~ "\u2265 q-value")
-        }
-        signif_labels[[i]] <- label
-      }
-      
-      breaks <- c(names(lc_colors), names(fia_colors))
-      values <- c(lc_colors,fia_colors)
-      names(values) <- NULL
-      
-      ## Background Scatter: Create data for background rects
-      rects_df <- p_data %>%
-        group_by(.data$Class) %>%
-        summarise(Start = min(.data$x)-1,
-                  End = max(.data$x)+1,
-                  Color = class_colors[unique(.data$Class)],
-                  n = n())
-      rects_df$Class <- factor(rects_df$Class, levels = breaks)
-      rects_df$Technique <- sapply(rects_df$Class, function(c) {
-        if (c %in% names(lc_colors)) {
-          technique <- 'LC'
-        } else if (c %in% names(fia_colors)) {
-          technique <- 'FIA'
-        } else {
-          technique <- NA
-        }
-        return(technique)
-      })
-      
-      ## Background Scatter: Determine border line between last LC and first FIA class
-      lc_fia_border <- p_data %>%
-        filter(.data$Class %in% names(lc_colors)) %>%
-        select(.data$x) %>%
-        max()
-      
-      
-      reactP_data(p_data)
-      reactRects_df(rects_df)
-      
-      reactLog2FCvulcano(log2FCvulcano)
       reactLog2FCTab(log2FCTab)
     } else {
       showModal(modalDialog(
@@ -452,153 +318,20 @@ server <- function(input, output, session) {
   })
   
   # Visualize log2(FC)
-  output$plotVolcano <- renderPlotly({
+  output$plotVolcano <- renderPlot({
     req(reactLog2FCTab())
     if ('Volcano plot' %in% input$plotLog2FC) {
-      ## Plot: Create ggplot object
-      p_fc <- ggplot(reactLog2FCvulcano(),
-                     aes(x = .data$log2FC,
-                         y = -log10(.data$qval),
-                         color = .data$Class,
-                         label = labels)) +
-        geom_vline(xintercept=c(-log2(1.5), log2(1.5)), col="black", linetype="dashed") +
-        geom_hline(yintercept=-log10(0.05), col="black", linetype="dashed") +
-        geom_point(size = 1, aes(text = paste0(Metabolite, "\nClass: ", Class, "\nlog2 Fold Change: ", round(log2FC, digits=5), "\np-value: ", round(pval, digits=5)))) +
-        scale_color_manual('Classes',
-                           breaks = breaks,
-                           values = values,
-                           drop = FALSE,
-                           guide = guide_legend(override.aes = list(size = 2),
-                                                order=2, ncol = 2)) +
-        theme(plot.title = element_text(face = 'bold.italic', hjust = 0.5),
-              legend.key = element_rect(fill = 'white')) +
-        labs(x = 'log2(FC)', y = "-log10(p)")
-      
-      ## Interactive: Create interactive plot
-      p <- ggplotly(p_fc, tooltip = "text")
-      p
+      MetAlyzer::plot_log2FC(reactLog2FCTab(), hide_labels_for = reactLog2FCTab()$Metabolite,
+                             vulcano = T)
     }
   })
-  output$plotScatter <- renderPlotly({
+  output$plotScatter <- renderPlot({
     req(reactLog2FCTab())
     if ('Scatter plot' %in% input$plotLog2FC) {
-      pre_plot <- ggplot(reactP_data(),
-                         aes(x = .data$x,
-                             y = .data$log2FC,
-                             color = .data$signif_color)) +
-        geom_point(size = 0.5)
-      
-      ylims <- ggplot_build(pre_plot)$layout$panel_params[[1]]$y.range
-      
-      ## Plot: Create ggplot object
-      p_fc <- ggplot(reactP_data(),
-                     aes(x = .data$x,
-                         y = .data$log2FC,
-                         color = .data$signif_color)) +
-        geom_rect(data = reactRects_df(),
-                  inherit.aes = FALSE,
-                  aes(xmin = .data$Start, xmax = .data$End,
-                      ymin = ylims[1], ymax = ylims[2],
-                      fill = .data$Class,
-                      text = paste0(Class, "\n", Technique, "\nNumber of Metabolites: ", n)),
-                  show.legend = TRUE,
-                  alpha = 0.4) +
-        geom_vline(xintercept = 0, linewidth = 0.5, color = 'black') +
-        geom_vline(xintercept = lc_fia_border+3, linewidth = 0.5, color = 'black', linetype="dotted") +
-        geom_hline(yintercept = 0, linewidth = 0.5, color = 'black') +
-        geom_point(size = 0.5, aes(text = paste0(Metabolite, 
-                                                 "\nClass: ", Class, 
-                                                 "\nlog2 Fold Change: ", round(log2FC, digits=5),  
-                                                 "\np-value: ", round(pval, digits=5)))) + 
-        scale_color_manual(paste0('Significance\n(linear model fit with FDR correction)'),
-                           labels = signif_labels,
-                           values = names(signif_colors),
-                           guide = guide_legend(order=1)) +
-        scale_fill_manual('Classes',
-                          breaks = breaks,
-                          values = values,
-                          drop = FALSE,
-                          guide = guide_legend(override.aes = list(alpha = 0.5),
-                                               order=2, ncol = 2)) +
-        theme(axis.ticks.x = element_blank(),
-              axis.text.x = element_blank(),
-              plot.title = element_text(face = 'bold.italic', hjust = 0.5),
-              legend.key = element_rect(fill = 'white'),
-              panel.grid.major.x = element_blank(),
-              panel.grid.major.y = element_line('#ECECEC'),
-              panel.grid.minor.x = element_blank(),
-              panel.grid.minor.y = element_line('#ECECEC'),
-              panel.background = element_blank()) +
-        labs(x = 'Metabolites')
-      
-      ## Interactive: Create interactive plot
-      p <- ggplotly(p_fc, tooltip = "text", showlegend = FALSE)
-      p <- hide_legend(p)
-      print("check")
-      p
+      MetAlyzer::plot_log2FC(reactLog2FCTab(), hide_labels_for = reactLog2FCTab()$Metabolite,
+                             vulcano = F)
     }
   })
-  output$plotScatterLegend <- renderPlot({
-    req(reactLog2FCTab())
-    if ('Scatter plot' %in% input$plotLog2FC) {
-      pre_plot <- ggplot(reactP_data(),
-                         aes(x = .data$x,
-                             y = .data$log2FC,
-                             color = .data$signif_color)) +
-        geom_point(size = 0.5)
-      
-      ylims <- ggplot_build(pre_plot)$layout$panel_params[[1]]$y.range
-      
-      ## Plot: Create ggplot object
-      p_fc <- ggplot(reactP_data(),
-                     aes(x = .data$x,
-                         y = .data$log2FC,
-                         color = .data$signif_color)) +
-        geom_rect(data = reactRects_df(),
-                  inherit.aes = FALSE,
-                  aes(xmin = .data$Start, xmax = .data$End,
-                      ymin = ylims[1], ymax = ylims[2],
-                      fill = .data$Class,
-                      text = paste0(Class, "\n", Technique, "\nNumber of Metabolites: ", n)),
-                  show.legend = TRUE,
-                  alpha = 0.4) +
-        geom_vline(xintercept = 0, linewidth = 0.5, color = 'black') +
-        geom_vline(xintercept = lc_fia_border+3, linewidth = 0.5, color = 'black', linetype="dotted") +
-        geom_hline(yintercept = 0, linewidth = 0.5, color = 'black') +
-        geom_point(size = 0.5, aes(text = paste0(Metabolite, 
-                                                 "\nClass: ", Class, 
-                                                 "\nlog2 Fold Change: ", round(log2FC, digits=5),  
-                                                 "\np-value: ", round(pval, digits=5)))) + 
-        scale_color_manual(paste0('Significance\n(linear model fit with FDR correction)'),
-                           labels = signif_labels,
-                           values = names(signif_colors),
-                           guide = guide_legend(order=1)) +
-        scale_fill_manual('Classes',
-                          breaks = breaks,
-                          values = values,
-                          drop = FALSE,
-                          guide = guide_legend(override.aes = list(alpha = 0.5),
-                                               order=2, ncol = 2)) +
-        theme(axis.ticks.x = element_blank(),
-              axis.text.x = element_blank(),
-              plot.title = element_text(face = 'bold.italic', hjust = 0.5),
-              legend.key = element_rect(fill = 'white'),
-              panel.grid.major.x = element_blank(),
-              panel.grid.major.y = element_line('#ECECEC'),
-              panel.grid.minor.x = element_blank(),
-              panel.grid.minor.y = element_line('#ECECEC'),
-              panel.background = element_blank()) +
-        labs(x = 'Metabolites')
-      
-      # Grab Legend ggplot
-      tmp <- ggplot_gtable(ggplot_build(p_fc))
-      leg <- which(sapply(tmp$grobs, function(x) x$name) ==  "guide-box")
-      legend <- tmp$grobs[[leg]]
-      legend <- grid.arrange(legend, ncol=1)
-      legend
-    }
-  })
-  
   output$plotNetwork <- renderPlot({
     req(reactLog2FCTab())
     if ('Network plot' %in% input$plotLog2FC) {
